e4d8a7c4608b36fa64a7d6420c63fdc61be28ce1
Chu <ttheko101@gmail.com>
2022-07-16 16:27:44 +0200

    lessons 15-22

---
+++ node-prac-1.js
@@ -0,0 +1,9 @@
+function findMeaning(){
+    return 42;
+}
+
+console.log(findMeaning())
+
+/*Node is a runtime not programming language*/
+/*Node is way of running JavaScript instructions on your computer outside the browser*/
+/* Common globals noe provides ~global,process,module __Filename, require() */
