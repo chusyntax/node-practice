11e88964dc5c456533180abd72e3f5b8f8b2b1c9
Chu <ttheko101@gmail.com>
2022-07-16 16:29:01 +0200

    added more code

--- node-prac-1.js
+++ node-prac-1.js
@@ -7,3 +7,7 @@ console.log(findMeaning())
 /*Node is a runtime not programming language*/
 /*Node is way of running JavaScript instructions on your computer outside the browser*/
 /* Common globals noe provides ~global,process,module __Filename, require() */
+// client = frontend, backend=server 
+//HTTP- Hyper text Transfer Protocol ~ Used for servers to communicate with client
+//JavaScript code executed in V8 will make use of built in Node.js APIs. When you call one of them,
+//They go through Node.js bindings, and get handled by libuv bindings
