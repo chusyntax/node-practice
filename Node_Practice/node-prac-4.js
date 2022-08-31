92b52364ff5cbbb5d22fec9636b1798c2d809225
Chu <ttheko101@gmail.com>
2022-08-31 23:33:18 +0200

    lessons 68-70

---
+++ Node_Practice/node-prac-4.js
@@ -0,0 +1,13 @@
+//Web Servers
+
+// API - Application Programming Interface
+
+//HTTP Requests -  Things we ask from our server
+//All HTTP Requests have four main parts
+
+// 1 ~ Method ~ Defines operation the browser wants to preforn on server (POST)
+// 2 ~ Path ~ The resource we are accessing on the backend (/messages)
+// 3 ~ Body ~ Contains data we are sending from browser to server (JSON)
+// 4 ~ Headers ~ Optional propertie you can specify on request to send additional metadata to server
+// Basically data about the data youa are sending (Size of data)
+//Every request must have the Jost header ~ Specifies which server your request is being sent to
