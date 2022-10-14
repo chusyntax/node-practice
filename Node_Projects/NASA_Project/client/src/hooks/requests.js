03032fe60af6ce42889399555b9c2e645c22e43d
Chu <ttheko101@gmail.com>
2022-10-14 15:15:06 +0200

    lesson 108

--- Node_Projects/NASA_Project/client/src/hooks/requests.js
+++ Node_Projects/NASA_Project/client/src/hooks/requests.js
@@ -1,8 +1,9 @@
-async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
-}
+const API_URL = 'http://localhost:8000'
 
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`)
+  return await response.json()
+}
 async function httpGetLaunches() {
   // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
--- Node_Projects/NASA_Project/server/src/app.js
+++ Node_Projects/NASA_Project/server/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
 const app = express();
+
+const planetsRouter = require('./routes/planets/planets.router')
+
 app.use(express.json())
+
+app.use(planetsRouter)
 
 module.exports = app
