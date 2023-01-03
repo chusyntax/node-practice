9ab0f68359a18afd9a5dc9d793b10351b213db47
Chu <ttheko101@gmail.com>
2023-01-03 19:42:14 +0200

    Lessons 117-118.

--- Node_Projects/NASA_Project/server/src/app.js
+++ Node_Projects/NASA_Project/server/src/app.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const morgan = require('morgan')
+
 const express = require('express');
 
 const cors = require('cors');
@@ -12,6 +14,13 @@ const planetsRouter = require('./routes/planets/planets.router');
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+
+//This type of Middlewear (Morgan) should go somewhere highup and right after any security related middlewear
+//It is used in the server files
+app.use(morgan('combined'))
+//Morgan is used for logging requests (in terminal). This gives us details about requests which we can use for debugging
+//This gives us a clearer picture as to what our users are going while accessing our application
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'..','public')))
 app.get('/',(req,res)=>{
